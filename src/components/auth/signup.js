5485245981088553158e055d5e06bb9d0998694b
PC\Adonis <adonis0444@gmail.com>
2019-05-04 16:12:04 -0400

    Create signin/signup route and components

---
+++ src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return(
+            <div className='signup'>
+                Up
+            </div>
+        )
+    }
+}
+
+export default Signup;
