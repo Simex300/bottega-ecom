c35a2161382224f03b2b774bd84327f07529f21c
PC\Adonis <adonis0444@gmail.com>
2019-05-04 15:53:25 -0400

    Building header component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './header/header';
+import Navbar from './header/navbar';
+
 class Layout extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='layout'>
+        <Header/>
+        <Navbar/>
         {this.props.children}
       </div>
     );
