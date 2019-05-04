b37bb02198f15263fe439520a49087188afe80ac
PC\Adonis <adonis0444@gmail.com>
2019-05-04 19:34:48 -0400

    Adding style to the signinForm component

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -11,6 +11,7 @@ class SigninForm extends Component {
             <form onSubmit={handleSubmit} className={`${className} signin-form`}>
                 <Field className='signin-form__email' name='email' title='Email' placeholder='Email' type='email' component={FormInput}/>
                 <Field className='signin-form__password' name='password' title='Password' placeholder='Password' type='password' component={FormInput}/>
+                <div className='signin-form__line'></div>
                 <Field className='signin-form__login-btn' onClick={() => console.log('Submitting Login!')} name='login' title='Login' type='submit' component={FormButton}/>
             </form>
         )
