913b497b708eb9dfb6aeb697be13ecb41cb3c5f9
PC\Adonis <adonis0444@gmail.com>
2019-05-04 16:39:08 -0400

    Create formField component and add some fields to SigninForm Component

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
+import { FormInput } from '../formFields';
+
 class SigninForm extends Component {
     render() {
         const { className } = this.props;
         return(
-            <div className={`${className} signin-form`}>
-                In
-            </div>
+            <form className={`${className} signin-form`}>
+                <Field className='signin-form__email' name='email' title='Email' placeholder='Email' type='email' component={FormInput}/>
+                <Field className='signin-form__password' name='password' title='Password' placeholder='Password' type='password' component={FormInput}/>
+            </form>
         )
     }
 }
