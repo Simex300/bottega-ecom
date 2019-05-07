e6e61515949bac16c325abc2d6fe93130b173fea
PC\Adonis <adonis0444@gmail.com>
2019-05-07 10:26:55 -0400

    Adding details component for the quick links in sign in

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -3,16 +3,34 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
+import Details from '../common/details';
+
+import history from '../../history';
 
 class SigninForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+        const links = [
+            {
+                title: 'Not registered? Create account here',
+                onClick: () => history.pushState('/signup')
+            },
+            {
+                title: 'Forgot account email?',
+                onClick: () => console.log("Forgot email")
+            },
+            {
+                title: 'Forgot password?',
+                onClick: () => console.log("Forgot password")
+            }
+        ]
         return(
             <form onSubmit={handleSubmit} className={`${className} signin-form`}>
                 <Field className='signin-form__email' name='email' title='Email' placeholder='Email' type='email' component={FormInput}/>
                 <Field className='signin-form__password' name='password' title='Password' placeholder='Password' type='password' component={FormInput}/>
                 <div className='signin-form__line'></div>
                 <Field className='signin-form__login-btn' onClick={() => console.log('Submitting Login!')} name='login' title='Login' type='submit' component={FormButton}/>
+                <Details className='signin-form__details' title='Quick Links' links={links} />
             </form>
         )
     }
