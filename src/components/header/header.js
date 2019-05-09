bef08b119546e56fa70547dc4645730cd148095b
PC\Adonis <adonis0444@gmail.com>
2019-05-08 21:18:44 -0400

    Adding default data to the navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 class Header extends Component {
     render() {
         return(
             <div className='header'>
                 <img src='http://via.placeholder.com/50x50' />
+                <div className='header__links'>
+                    {
+                        this.props.headerLinks.map((link, index) => {
+                            return (
+                                <a key={index} className='navbar__link' onClick={() => console.log("Switchin Tabs")}>{link.title}</a>
+                            )
+                        })
+                    }
+                </div>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    const { headerLinks } = state.headerNavbar;
+    return {
+        headerLinks
+    }
+}
+
+Navbar = connect(mapStateToProps)(Navbar);
 
 export default Header;
--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 class Navbar extends Component {
     render() {
         return(
             <div className='navbar'>
-                
+                {
+                    this.props.navbarLinks.map((link, index) => {
+                        return (
+                            <a key={index} className='navbar__link' onClick={() => console.log("Switchin Tabs")}>{link.title}</a>
+                        )
+                    })
+                }
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    const { navbarLinks } = state.headerNavbar;
+    return {
+        navbarLinks
+    }
+}
+
+Navbar = connect(mapStateToProps)(Navbar);
 
 export default Navbar;
