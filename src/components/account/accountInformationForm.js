34e87feb79c414677832d753b761cbea2b2c5694
PC\Adonis <adonis0444@gmail.com>
2019-05-12 10:16:10 -0400

    Creating the form grid to the Account information form component

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -10,11 +10,19 @@ class AccountInformationForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
         return(
-            <form onSubmit={handleSubmit} className={`${className} signin-form`}>
-                <Field className='signin-form__email' name='email' title='Email' placeholder='Email' type='email' component={FormInput}/>
-                <Field className='signin-form__password' name='password' title='Password' placeholder='Password' type='password' component={FormInput}/>
-                <div className='signin-form__line'></div>
-                <Field className='signin-form__login-btn' onClick={() => history.push('/account')} name='login' title='Login' type='submit' component={FormButton}/>
+            <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+                <Field className='account-information-form__email' name='email' title='Email' placeholder='Email' type='email' component={FormInput}/>
+                <Field className='account-information-form__name' name='name' title='Name' placeholder='Name' type='text' component={FormInput}/>
+                <Field className='account-information-form__street-address' name='street-address' title='Street Address' placeholder='Street Address' type='text' component={FormInput}/>
+                <Field className='account-information-form__city' name='city' title='City' placeholder='City' type='text' component={FormInput}/>
+
+                <Field className='account-information-form__state' name='state' title='State' placeholder='State' type='text' component={FormInput}/>
+                <Field className='account-information-form__zipcode' name='zipcode' title='Zipcode' placeholder='Zipcode' type='text' component={FormInput}/>
+
+                
+                {/* <Field className='account-information-form__password' name='password' title='Password' placeholder='Password' type='password' component={FormInput}/>
+                <div className='account-information-form__line'></div>
+                <Field className='account-information-form__login-btn' onClick={() => history.push('/account')} name='login' title='Login' type='submit' component={FormButton}/> */}
             </form>
         )
     }
@@ -25,3 +33,4 @@ AccountInformationForm = reduxForm({
 })(AccountInformationForm);
 
 export default AccountInformationForm;
+
