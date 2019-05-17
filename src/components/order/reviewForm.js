d1b7ab0b0a6ad37d41b8d5adf074ca036e1df4dd
PC\Adonis <adonis0444@gmail.com>
2019-05-17 14:45:28 -0400

    Creating review detail component and styling it

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -5,16 +5,40 @@ import history from '../../history';
 
 import { FormButton } from '../formFields';
 import ReviewProducts from './reviewProducts';
+import GreenPriceTag from '../common/greenPriceTag';
 
 class ReviewForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
         return(
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                <div className='review-form__legend'>
+                    <div className='review-form__legend__name'>Name</div>
+                    <div className='review-form__legend__quantity'>Quantity</div>
+                    <div className='review-form__legend__price'>Price</div>
+                </div>
                 <ReviewProducts className='review-form__products' />
                 <div className='review-form__line'></div>
                 <Field className='review-form__proceed' onClick={() => history.push('/signin')} name='proceed' title='Proceed to Checkout' type='submit' component={FormButton}/>
                 <Field className='review-form__back' onClick={() => history.push('/shop')} name='back' title='Back' type='button' short={true} component={FormButton}/>
+                <div className='review-form__details review-details'>
+                    <div className='review-details__subtotal review-detail'>
+                        <div className='review-detail__title'>Subtotal: </div>
+                        <div className='review-detail__price'>$7.96</div>
+                    </div>
+                    <div className='review-details__tax review-detail'>
+                        <div className='review-detail__title'>Tax: </div>
+                        <div className='review-detail__price'>$0.16</div>
+                    </div>
+                    <div className='review-details__shipping review-detail'>
+                        <div className='review-detail__title'>Shipping: </div>
+                        <div className='review-detail__price'>$0.00</div>
+                    </div>
+                    <div className='review-details__shipping review-detail review-detail-green'>
+                        <div className='review-detail__title'>Total: </div>
+                        <div className='review-detail__price'>$8.12</div>
+                    </div>
+                </div>
             </form>
         )
     }
