cbaec24ccbfd409dd9b8cb3b12ab41ee819a05b3
PC\Adonis <adonis0444@gmail.com>
2019-05-17 17:56:14 -0400

    Creating Order Summary component and some subcomponents

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -7,21 +7,12 @@ import Details from '../common/details';
 
 import history from '../../history';
 
+import OrderSummary from './orderSummary';
+
 class ShippingForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-        const info = [
-            {
-                title: 'At least 6 characters'
-            },
-            {
-                title: 'At least one number'
-            },
-            {
-                title: 'At least one symbol'
-            }
-        ]
-        return(
+        return (
             <form onSubmit={handleSubmit} className={`${className} shipping-form`}>
                 <Field className='shipping-form__name' name='name' title='Name' placeholder='Name' type='text' component={FormInput}/>
                 <Field className='shipping-form__address' name='street-address' title='Street Address' placeholder='Street Address' type='text' component={FormInput}/>
@@ -32,7 +23,7 @@ class ShippingForm extends Component {
                 <div className='shipping-form__line'></div>
                 <Field className='shipping-form__confirm' onClick={() => history.push('/infomation-payment')} name='confirm' title='Confirm Address' type='submit' component={FormButton}/>
                 <Field className='shipping-form__back' onClick={() => history.push('/')} name='back' title='Back' type='button' short={true} component={FormButton}/>
-                <Details className='shipping-form__details' title='Password Requirements' infos={info} />
+                <OrderSummary className='shipping-form__details'/>
             </form>
         )
     }
